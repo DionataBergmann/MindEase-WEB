b182f12ee90c104d87eb345aa1787d389a4bd8e6
DionataBergmann <dionata.podewils@outlook.com>
2026-02-21 16:46:46 -0300

    :wrench: feat: initial config

---
+++ next.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Next 16: Turbopack é o default. Se precisar do webpack (ex.: Windows path): next dev --webpack
+};
+
+export default nextConfig;
